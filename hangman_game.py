adb562ba58edc06d426152c27291b4d9d06f1adb
Alex88ey88 <lubencovaleksey@gmail.com>
2025-05-21 23:15:23 +0500

    hangman game is ready

---
+++ hangman_game.py
@@ -0,0 +1,39 @@
+"""
+Простой аналог игры Hangman с висилицей.
+Здесь представлено несколько слов,
+при желании их можно увеличить и будет интереснее,
+Так-же можно уменьшить количество жизней (attempts)
+
+"""
+
+import random
+
+words = ['python', 'java', 'kotlin', 'javascript', 'ruby', 'swift']
+
+choisen_word = random.choice(words)
+
+word_display = ['_' for _ in choisen_word]
+
+attempts = 8 # Максимальное количество ошибок при ушадывании слова
+
+while attempts > 0 and '_' in word_display:
+    print("\n" + ' '.join(word_display))
+
+    guess = input("Угадайте букву в слове: ").lower()
+
+    if guess in choisen_word:
+        for index, letter in enumerate(choisen_word):
+            if letter == guess:
+                word_display[index] = guess # Показываем букву на экране
+    else:
+        print("Такой буквы не существует в данном слове.")
+        attempts -= 1
+
+# Заключение в игре
+if '_' not in word_display:
+    print("Вы угодали слово!")
+    print(' '.join(word_display))
+    print("Вы смогли выжить")
+else:
+    print(f"Вы потратили все жизни и не смогли разгадать слово: {choisen_word}")
+    print("Вы ПРОИГРАЛИ")
